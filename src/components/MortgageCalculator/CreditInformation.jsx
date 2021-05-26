ee097c1e3a54b59a1bda72454e33f34d1ffc05ba
serviktor050 <mr.s-gagulin@yandex.ru>
2021-05-26 15:37:52 +0300

    Реализован вывод данных в блок Информация о кредите для формы недвижимость

--- src/components/MortgageCalculator/CreditInformation.jsx
+++ src/components/MortgageCalculator/CreditInformation.jsx
@@ -1,24 +1,60 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getMonthlyPayment } from "../../CalculationFunctions/getMonthlyPayment";
+import { getPercentages } from "../../CalculationFunctions/getPercentages";
 
 export default function CreditInformation() {
+  const { costOfRealEstate, initialPayment, loanTerm, interestRate } =
+    useSelector((state) => state.mortgageCalculatorForm);
+
+  let setMonthlyPayment = getMonthlyPayment(
+    Number(costOfRealEstate) - Number(initialPayment),
+    Number(loanTerm),
+    Number(interestRate)
+  );
+
+  const koeff = 1.667;
+
+  let requiredIncome = Number((setMonthlyPayment * koeff).toFixed());
+
   return (
     <>
       <div className="info-value">
         <div className="info-value-item">
           <div className="item-name">Кредит</div>
-          <div className="item-value"> ₽</div>
+          <div className="item-value">
+            {Number(costOfRealEstate) - Number(initialPayment)} ₽
+          </div>
         </div>
         <div className="info-value-item">
           <div className="item-name">Проценты</div>
-          <div className="item-value"> ₽</div>
+          <div className="item-value">
+            {getPercentages(
+              Number(costOfRealEstate) - Number(initialPayment),
+              setMonthlyPayment,
+              interestRate,
+              loanTerm
+            )}
+            ₽
+          </div>
         </div>
         <div className="info-value-item">
           <div className="item-name">Проценты + Кредит</div>
-          <div className="item-value"> ₽</div>
+          <div className="item-value">
+            {Number(costOfRealEstate) -
+              Number(initialPayment) +
+              getPercentages(
+                Number(costOfRealEstate) - Number(initialPayment),
+                setMonthlyPayment,
+                interestRate,
+                loanTerm
+              )}
+            ₽
+          </div>
         </div>
         <div className="info-value-item">
           <div className="item-name">Необходимый доход</div>
-          <div className="item-value"> ₽</div>
+          <div className="item-value"> {requiredIncome}₽</div>
         </div>
       </div>
       <div className="submit-form">

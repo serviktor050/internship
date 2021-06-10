8fec53cbed3d766b5ede0cf9640b5977a6682e2e
serviktor050 <mr.s-gagulin@yandex.ru>
2021-06-10 17:36:39 +0300

    Добавлен примерный вариант визуального отображения таймера

--- src/components/Pages/Timer.jsx
+++ src/components/Pages/Timer.jsx
@@ -9,26 +9,44 @@ export default function Timer() {
   const [timer, setTimer] = useState("");
   const [btnStatus, setBtnStatus] = useState(false);
 
+  const setProgress = (seconds) => {
+    const offset = circumference - (seconds / 60) * circumference;
+    circle.style.strokeDashoffset = offset;
+  };
+
   useEffect(() => {
     if (btnStatus) {
       setTimeout(() => {
         if (timer > 0) {
-          return setTimer(timer - 1);
+          setTimer(timer - 1);
+          setProgress(timer);
         }
       }, 1000);
     }
   }, [timer, btnStatus]);
 
   const handleChange = (evt) => {
-    setChangeTime(Number(evt.target.value));
+    setChangeTime(evt.target.value);
   };
+
+  let circle;
+  let radius;
+  let circumference;
+
+  if (timer !== "" && timer !== 0) {
+    circle = document.querySelector(".progress-ring__circle");
+    radius = circle.r.baseVal.value;
+    circumference = 2 * Math.PI * radius;
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = circumference;
+  }
 
   return (
     <>
       {userToken === null && <Redirect to="/" />}
       <h1>Таймер</h1>
       <p>Установите время:</p>
-      <input type="text" onChange={handleChange} value={changeTime} />
+      <input type="number" onChange={handleChange} value={changeTime} />
       <button
         onClick={() => {
           setTimer(changeTime);
@@ -37,9 +55,25 @@ export default function Timer() {
       >
         Установить
       </button>
-      {timer !== 0 && <div>{timer}</div>}
+      {!btnStatus && timer !== 0 && timer !== null && (
+        <>
+          <div>{timer}</div>
+          <svg className="progress-ring" width="160" height="160">
+            <circle
+              className="progress-ring__circle"
+              stroke="red"
+              strokeWidth="12"
+              cx="80"
+              cy="80"
+              r="70"
+              fill="transparent"
+            />
+            <text> Start </text>
+          </svg>
+        </>
+      )}
       {timer === 0 && <div>Время вышло!</div>}
-      {!btnStatus && timer !== "" && (
+      {!btnStatus && timer !== "" && timer !== 0 && (
         <button onClick={() => setBtnStatus((prev) => !prev)}>Cтарт</button>
       )}
       {btnStatus && (
